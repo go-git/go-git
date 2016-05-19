6a3ba7037ebf047fcdad3745dc1f684ccb472dce
Máximo Cuadros <mcuadros@gmail.com>
2016-05-19 16:48:23 +0200

    documentation

--- formats/pktline/encoder.go
+++ formats/pktline/encoder.go
@@ -7,7 +7,7 @@ import (
 )
 
 var (
-	//ErrOverflow happends when the line length exceed the MaxLength
+	//ErrOverflow is triggered when the line length exceed the MaxLength
 	ErrOverflow = errors.New("unexpected string length (overflow)")
 )
 
