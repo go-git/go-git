6c6a37b9128189ba4cdde8128428a36ef75d1a44
Máximo Cuadros <mcuadros@gmail.com>
2016-10-18 12:03:34 +0200

    travis: fix ssh key

--- .travis/install_key.sh
+++ .travis/install_key.sh
@@ -1,4 +1,3 @@
 openssl aes-256-cbc -K $encrypted_1477e58fe67a_key -iv $encrypted_1477e58fe67a_iv -in .travis/deploy.pem.enc -out .travis/deploy.pem -d
-eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy.pem
 ssh-add .travis/deploy.pem
