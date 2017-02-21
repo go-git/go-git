0b8b8da617d5a077f282e57d0300dc106a604236
Alberto Cortés <alcortesm@gmail.com>
2017-02-21 10:24:10 +0100

    difftree for git.Trees (#273)
    Last PR to fix #82:
    
    This PR modifies the difftree package itself. The old version extracted the files in both trees and compare them by hand. The new version turn the trees into merkletrie.Noders and call the merkletrie.Difftree function on them.
    
    How to review this PR:
    
    treenoder.go: defines the treeNoder type that wraps a git.Tree and implements merkletrie.Noder.
    change.go: defines the type of the output of a difftree operation. The type is the same as before, but I have moved it into its own file to keep the package organized. The old package defines the Action type too (insert, delete, modify), now, we reuse merkletrie.Action and it is no longer a field, but a method.
    change_adaptor.go: defines functions to turn merkletrie.Changes into difftree.Changes.
    difftree.go: before this patch this file holds all the logic to do a difftree, now it just turns the git.Trees into treeNoders, call merkletrie.difftree on them, and turns the resulting merkletrie.Changes into difftree.Changes.
    The only interesting piece of code here is that noders don't have the concept of mode (file permissions). The treenoder type codifies git.Tree modes into the merkletrie.Noder hash, so changes in the mode of a file are detected as modifications, just as the original git diff-tree command does.

--- plumbing/difftree/difftree_test.go
+++ plumbing/difftree/difftree_test.go
@@ -4,14 +4,15 @@ import (
 	"sort"
 	"testing"
 
-	"github.com/src-d/go-git-fixtures"
 	"srcd.works/go-git.v4/plumbing"
 	"srcd.works/go-git.v4/plumbing/format/packfile"
 	"srcd.works/go-git.v4/plumbing/object"
 	"srcd.works/go-git.v4/plumbing/storer"
 	"srcd.works/go-git.v4/storage/filesystem"
 	"srcd.works/go-git.v4/storage/memory"
+	"srcd.works/go-git.v4/utils/merkletrie"
 
+	"github.com/src-d/go-git-fixtures"
 	. "gopkg.in/check.v1"
 )
 
@@ -31,12 +32,6 @@ func (s *DiffTreeSuite) SetUpSuite(c *C) {
 	c.Assert(err, IsNil)
 	s.Storer = sto
 	s.cache = make(map[string]storer.EncodedObjectStorer)
-}
-
-func (s *DiffTreeSuite) tree(c *C, h plumbing.Hash) *object.Tree {
-	t, err := object.GetTree(s.Storer, h)
-	c.Assert(err, IsNil)
-	return t
 }
 
 func (s *DiffTreeSuite) commitFromStorer(c *C, sto storer.EncodedObjectStorer,
@@ -76,108 +71,48 @@ func (s *DiffTreeSuite) storageFromPackfile(f *fixtures.Fixture) storer.EncodedO
 
 var _ = Suite(&DiffTreeSuite{})
 
-func (s *DiffTreeSuite) TestActionString(c *C) {
-	expected := "Insert"
-	action := Insert
-	obtained := action.String()
-	c.Assert(obtained, Equals, expected)
-
-	expected = "Delete"
-	action = Delete
-	obtained = action.String()
-	c.Assert(obtained, Equals, expected)
-
-	expected = "Modify"
-	action = Modify
-	obtained = action.String()
-	c.Assert(obtained, Equals, expected)
-
-	action = 37
-	c.Assert(func() { _ = action.String() },
-		PanicMatches, "unsupported action: 37")
-}
-
-func (s *DiffTreeSuite) TestChangeFilesInsert(c *C) {
-	tree := s.tree(c, plumbing.NewHash("a8d315b2b1c615d43042c3a62402b8a54288cf5c"))
-
-	change := &Change{Action: Insert}
-	change.To.Name = "json/long.json"
-	change.To.Tree = tree
-	change.To.TreeEntry.Hash = plumbing.NewHash("49c6bb89b17060d7b4deacb7b338fcc6ea2352a9")
-
-	from, to, err := change.Files()
-	c.Assert(err, IsNil)
-	c.Assert(from, IsNil)
-	c.Assert(to.ID(), Equals, change.To.TreeEntry.Hash)
-}
-
-func (s *DiffTreeSuite) TestChangeFilesDelete(c *C) {
-	tree := s.tree(c, plumbing.NewHash("a8d315b2b1c615d43042c3a62402b8a54288cf5c"))
-
-	change := &Change{Action: Delete}
-	change.From.Name = "json/long.json"
-	change.From.Tree = tree
-	change.From.TreeEntry.Hash = plumbing.NewHash("49c6bb89b17060d7b4deacb7b338fcc6ea2352a9")
-
-	from, to, err := change.Files()
-	c.Assert(err, IsNil)
-	c.Assert(to, IsNil)
-	c.Assert(from.ID(), Equals, change.From.TreeEntry.Hash)
-}
-
-func (s *DiffTreeSuite) TestChangeFilesModify(c *C) {
-	tree := s.tree(c, plumbing.NewHash("a8d315b2b1c615d43042c3a62402b8a54288cf5c"))
-
-	change := &Change{Action: Modify}
-	change.To.Name = "json/long.json"
-	change.To.Tree = tree
-	change.To.TreeEntry.Hash = plumbing.NewHash("49c6bb89b17060d7b4deacb7b338fcc6ea2352a9")
-	change.From.Name = "json/long.json"
-	change.From.Tree = tree
-	change.From.TreeEntry.Hash = plumbing.NewHash("9a48f23120e880dfbe41f7c9b7b708e9ee62a492")
-
-	from, to, err := change.Files()
-	c.Assert(err, IsNil)
-	c.Assert(to.ID(), Equals, change.To.TreeEntry.Hash)
-	c.Assert(from.ID(), Equals, change.From.TreeEntry.Hash)
-}
-
-func (s *DiffTreeSuite) TestChangeString(c *C) {
-	expected := "<Action: Insert, Path: foo>"
-	change := &Change{Action: Insert}
-	change.From.Name = "foo"
-
-	obtained := change.String()
-	c.Assert(obtained, Equals, expected)
-}
-
-func (s *DiffTreeSuite) TestChangesString(c *C) {
-	expected := "[]"
-	changes := newEmpty()
-	obtained := changes.String()
-	c.Assert(obtained, Equals, expected)
-
-	expected = "[<Action: Modify, Path: bla>]"
-	changes = make([]*Change, 1)
-	changes[0] = &Change{Action: Modify}
-	changes[0].From.Name = "bla"
-
-	obtained = changes.String()
-	c.Assert(obtained, Equals, expected)
-
-	expected = "[<Action: Modify, Path: bla>, <Action: Insert, Path: foo/bar>]"
-	changes = make([]*Change, 2)
-	changes[0] = &Change{Action: Modify}
-	changes[0].From.Name = "bla"
-	changes[1] = &Change{Action: Insert}
-	changes[1].From.Name = "foo/bar"
-	obtained = changes.String()
-	c.Assert(obtained, Equals, expected)
-}
-
 type expectChange struct {
-	Action Action
+	Action merkletrie.Action
 	Name   string
+}
+
+func assertChanges(a Changes, c *C) {
+	for _, changes := range a {
+		action, err := changes.Action()
+		c.Assert(err, IsNil)
+		switch action {
+		case merkletrie.Insert:
+			c.Assert(changes.From.Tree, IsNil)
+			c.Assert(changes.To.Tree, NotNil)
+		case merkletrie.Delete:
+			c.Assert(changes.From.Tree, NotNil)
+			c.Assert(changes.To.Tree, IsNil)
+		case merkletrie.Modify:
+			c.Assert(changes.From.Tree, NotNil)
+			c.Assert(changes.To.Tree, NotNil)
+		default:
+			c.Fatalf("unknown action: %d", action)
+		}
+	}
+}
+
+func equalChanges(a Changes, b []expectChange, c *C) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	sort.Sort(a)
+
+	for i, va := range a {
+		vb := b[i]
+		action, err := va.Action()
+		c.Assert(err, IsNil)
+		if action != vb.Action || va.name() != vb.Name {
+			return false
+		}
+	}
+
+	return true
 }
 
 func (s *DiffTreeSuite) TestDiffTree(c *C) {
@@ -186,186 +121,209 @@ func (s *DiffTreeSuite) TestDiffTree(c *C) {
 		commit1    string         // the commit of the first tree
 		commit2    string         // the commit of the second tree
 		expected   []expectChange // the expected list of []changeExpect
-	}{{
-		"https://github.com/dezfowler/LiteMock.git",
-		"",
-		"",
-		[]expectChange{},
-	}, {
-		"https://github.com/dezfowler/LiteMock.git",
-		"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
-		"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
-		[]expectChange{},
-	}, {
-		"https://github.com/dezfowler/LiteMock.git",
-		"",
-		"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
-		[]expectChange{
-			{Action: Insert, Name: "README"},
+	}{
+		{
+			"https://github.com/dezfowler/LiteMock.git",
+			"",
+			"",
+			[]expectChange{},
 		},
-	}, {
-		"https://github.com/dezfowler/LiteMock.git",
-		"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
-		"",
-		[]expectChange{
-			{Action: Delete, Name: "README"},
+		{
+			"https://github.com/dezfowler/LiteMock.git",
+			"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
+			"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
+			[]expectChange{},
 		},
-	}, {
-		"https://github.com/githubtraining/example-branches.git",
-		"",
-		"f0eb272cc8f77803478c6748103a1450aa1abd37",
-		[]expectChange{
-			{Action: Insert, Name: "README.md"},
+		{
+			"https://github.com/dezfowler/LiteMock.git",
+			"",
+			"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
+			[]expectChange{
+				{Action: merkletrie.Insert, Name: "README"},
+			},
 		},
-	}, {
-		"https://github.com/githubtraining/example-branches.git",
-		"f0eb272cc8f77803478c6748103a1450aa1abd37",
-		"",
-		[]expectChange{
-			{Action: Delete, Name: "README.md"},
+		{
+			"https://github.com/dezfowler/LiteMock.git",
+			"b7965eaa2c4f245d07191fe0bcfe86da032d672a",
+			"",
+			[]expectChange{
+				{Action: merkletrie.Delete, Name: "README"},
+			},
 		},
-	}, {
-		"https://github.com/githubtraining/example-branches.git",
-		"f0eb272cc8f77803478c6748103a1450aa1abd37",
-		"f0eb272cc8f77803478c6748103a1450aa1abd37",
-		[]expectChange{},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"",
-		"9608eed92b3839b06ebf72d5043da547de10ce85",
-		[]expectChange{
-			{Action: Insert, Name: "README"},
-			{Action: Insert, Name: "gem_builder.rb"},
-			{Action: Insert, Name: "gem_eval.rb"},
+		{
+			"https://github.com/githubtraining/example-branches.git",
+			"",
+			"f0eb272cc8f77803478c6748103a1450aa1abd37",
+			[]expectChange{
+				{Action: merkletrie.Insert, Name: "README.md"},
+			},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"9608eed92b3839b06ebf72d5043da547de10ce85",
-		"",
-		[]expectChange{
-			{Action: Delete, Name: "README"},
-			{Action: Delete, Name: "gem_builder.rb"},
-			{Action: Delete, Name: "gem_eval.rb"},
+		{
+			"https://github.com/githubtraining/example-branches.git",
+			"f0eb272cc8f77803478c6748103a1450aa1abd37",
+			"",
+			[]expectChange{
+				{Action: merkletrie.Delete, Name: "README.md"},
+			},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"9608eed92b3839b06ebf72d5043da547de10ce85",
-		"9608eed92b3839b06ebf72d5043da547de10ce85",
-		[]expectChange{},
-	}, {
-		"https://github.com/toqueteos/ts3.git",
-		"",
-		"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
-		[]expectChange{
-			{Action: Insert, Name: "README.markdown"},
-			{Action: Insert, Name: "examples/bot.go"},
-			{Action: Insert, Name: "examples/raw_shell.go"},
-			{Action: Insert, Name: "helpers.go"},
-			{Action: Insert, Name: "ts3.go"},
+		{
+			"https://github.com/githubtraining/example-branches.git",
+			"f0eb272cc8f77803478c6748103a1450aa1abd37",
+			"f0eb272cc8f77803478c6748103a1450aa1abd37",
+			[]expectChange{},
 		},
-	}, {
-		"https://github.com/toqueteos/ts3.git",
-		"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
-		"",
-		[]expectChange{
-			{Action: Delete, Name: "README.markdown"},
-			{Action: Delete, Name: "examples/bot.go"},
-			{Action: Delete, Name: "examples/raw_shell.go"},
-			{Action: Delete, Name: "helpers.go"},
-			{Action: Delete, Name: "ts3.go"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"",
+			"9608eed92b3839b06ebf72d5043da547de10ce85",
+			[]expectChange{
+				{Action: merkletrie.Insert, Name: "README"},
+				{Action: merkletrie.Insert, Name: "gem_builder.rb"},
+				{Action: merkletrie.Insert, Name: "gem_eval.rb"},
+			},
 		},
-	}, {
-		"https://github.com/toqueteos/ts3.git",
-		"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
-		"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
-		[]expectChange{},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"9608eed92b3839b06ebf72d5043da547de10ce85",
-		"6c41e05a17e19805879689414026eb4e279f7de0",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"9608eed92b3839b06ebf72d5043da547de10ce85",
+			"",
+			[]expectChange{
+				{Action: merkletrie.Delete, Name: "README"},
+				{Action: merkletrie.Delete, Name: "gem_builder.rb"},
+				{Action: merkletrie.Delete, Name: "gem_eval.rb"},
+			},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"6c41e05a17e19805879689414026eb4e279f7de0",
-		"89be3aac2f178719c12953cc9eaa23441f8d9371",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
-			{Action: Insert, Name: "gem_eval_test.rb"},
-			{Action: Insert, Name: "security.rb"},
-			{Action: Insert, Name: "security_test.rb"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"9608eed92b3839b06ebf72d5043da547de10ce85",
+			"9608eed92b3839b06ebf72d5043da547de10ce85",
+			[]expectChange{},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"89be3aac2f178719c12953cc9eaa23441f8d9371",
-		"597240b7da22d03ad555328f15abc480b820acc0",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
+		{
+			"https://github.com/toqueteos/ts3.git",
+			"",
+			"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
+			[]expectChange{
+				{Action: merkletrie.Insert, Name: "README.markdown"},
+				{Action: merkletrie.Insert, Name: "examples/bot.go"},
+				{Action: merkletrie.Insert, Name: "examples/raw_shell.go"},
+				{Action: merkletrie.Insert, Name: "helpers.go"},
+				{Action: merkletrie.Insert, Name: "ts3.go"},
+			},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"597240b7da22d03ad555328f15abc480b820acc0",
-		"0260380e375d2dd0e1a8fcab15f91ce56dbe778e",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
-			{Action: Modify, Name: "gem_eval_test.rb"},
-			{Action: Insert, Name: "lazy_dir.rb"},
-			{Action: Insert, Name: "lazy_dir_test.rb"},
-			{Action: Modify, Name: "security.rb"},
-			{Action: Modify, Name: "security_test.rb"},
+		{
+			"https://github.com/toqueteos/ts3.git",
+			"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
+			"",
+			[]expectChange{
+				{Action: merkletrie.Delete, Name: "README.markdown"},
+				{Action: merkletrie.Delete, Name: "examples/bot.go"},
+				{Action: merkletrie.Delete, Name: "examples/raw_shell.go"},
+				{Action: merkletrie.Delete, Name: "helpers.go"},
+				{Action: merkletrie.Delete, Name: "ts3.go"},
+			},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"0260380e375d2dd0e1a8fcab15f91ce56dbe778e",
-		"597240b7da22d03ad555328f15abc480b820acc0",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
-			{Action: Modify, Name: "gem_eval_test.rb"},
-			{Action: Delete, Name: "lazy_dir.rb"},
-			{Action: Delete, Name: "lazy_dir_test.rb"},
-			{Action: Modify, Name: "security.rb"},
-			{Action: Modify, Name: "security_test.rb"},
+		{
+			"https://github.com/toqueteos/ts3.git",
+			"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
+			"764e914b75d6d6df1fc5d832aa9840f590abf1bb",
+			[]expectChange{},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"0260380e375d2dd0e1a8fcab15f91ce56dbe778e",
-		"ca9fd470bacb6262eb4ca23ee48bb2f43711c1ff",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
-			{Action: Modify, Name: "security.rb"},
-			{Action: Modify, Name: "security_test.rb"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"9608eed92b3839b06ebf72d5043da547de10ce85",
+			"6c41e05a17e19805879689414026eb4e279f7de0",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+			},
 		},
-	}, {
-		"https://github.com/github/gem-builder.git",
-		"fe3c86745f887c23a0d38c85cfd87ca957312f86",
-		"b7e3f636febf7a0cd3ab473b6d30081786d2c5b6",
-		[]expectChange{
-			{Action: Modify, Name: "gem_eval.rb"},
-			{Action: Modify, Name: "gem_eval_test.rb"},
-			{Action: Insert, Name: "git_mock"},
-			{Action: Modify, Name: "lazy_dir.rb"},
-			{Action: Modify, Name: "lazy_dir_test.rb"},
-			{Action: Modify, Name: "security.rb"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"6c41e05a17e19805879689414026eb4e279f7de0",
+			"89be3aac2f178719c12953cc9eaa23441f8d9371",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+				{Action: merkletrie.Insert, Name: "gem_eval_test.rb"},
+				{Action: merkletrie.Insert, Name: "security.rb"},
+				{Action: merkletrie.Insert, Name: "security_test.rb"},
+			},
 		},
-	}, {
-		"https://github.com/rumpkernel/rumprun-xen.git",
-		"1831e47b0c6db750714cd0e4be97b5af17fb1eb0",
-		"51d8515578ea0c88cc8fc1a057903675cf1fc16c",
-		[]expectChange{
-			{Action: Modify, Name: "Makefile"},
-			{Action: Modify, Name: "netbsd_init.c"},
-			{Action: Modify, Name: "rumphyper_stubs.c"},
-			{Action: Delete, Name: "sysproxy.c"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"89be3aac2f178719c12953cc9eaa23441f8d9371",
+			"597240b7da22d03ad555328f15abc480b820acc0",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+			},
 		},
-	}, {
-		"https://github.com/rumpkernel/rumprun-xen.git",
-		"1831e47b0c6db750714cd0e4be97b5af17fb1eb0",
-		"e13e678f7ee9badd01b120889e0ec5fdc8ae3802",
-		[]expectChange{
-			{Action: Modify, Name: "app-tools/rumprun"},
+		{
+			"https://github.com/github/gem-builder.git",
+			"597240b7da22d03ad555328f15abc480b820acc0",
+			"0260380e375d2dd0e1a8fcab15f91ce56dbe778e",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+				{Action: merkletrie.Modify, Name: "gem_eval_test.rb"},
+				{Action: merkletrie.Insert, Name: "lazy_dir.rb"},
+				{Action: merkletrie.Insert, Name: "lazy_dir_test.rb"},
+				{Action: merkletrie.Modify, Name: "security.rb"},
+				{Action: merkletrie.Modify, Name: "security_test.rb"},
+			},
 		},
-	}} {
+		{
+			"https://github.com/github/gem-builder.git",
+			"0260380e375d2dd0e1a8fcab15f91ce56dbe778e",
+			"597240b7da22d03ad555328f15abc480b820acc0",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+				{Action: merkletrie.Modify, Name: "gem_eval_test.rb"},
+				{Action: merkletrie.Delete, Name: "lazy_dir.rb"},
+				{Action: merkletrie.Delete, Name: "lazy_dir_test.rb"},
+				{Action: merkletrie.Modify, Name: "security.rb"},
+				{Action: merkletrie.Modify, Name: "security_test.rb"},
+			},
+		},
+		{
+			"https://github.com/github/gem-builder.git",
+			"0260380e375d2dd0e1a8fcab15f91ce56dbe778e",
+			"ca9fd470bacb6262eb4ca23ee48bb2f43711c1ff",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+				{Action: merkletrie.Modify, Name: "security.rb"},
+				{Action: merkletrie.Modify, Name: "security_test.rb"},
+			},
+		},
+		{
+			"https://github.com/github/gem-builder.git",
+			"fe3c86745f887c23a0d38c85cfd87ca957312f86",
+			"b7e3f636febf7a0cd3ab473b6d30081786d2c5b6",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "gem_eval.rb"},
+				{Action: merkletrie.Modify, Name: "gem_eval_test.rb"},
+				{Action: merkletrie.Insert, Name: "git_mock"},
+				{Action: merkletrie.Modify, Name: "lazy_dir.rb"},
+				{Action: merkletrie.Modify, Name: "lazy_dir_test.rb"},
+				{Action: merkletrie.Modify, Name: "security.rb"},
+			},
+		},
+		{
+			"https://github.com/rumpkernel/rumprun-xen.git",
+			"1831e47b0c6db750714cd0e4be97b5af17fb1eb0",
+			"51d8515578ea0c88cc8fc1a057903675cf1fc16c",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "Makefile"},
+				{Action: merkletrie.Modify, Name: "netbsd_init.c"},
+				{Action: merkletrie.Modify, Name: "rumphyper_stubs.c"},
+				{Action: merkletrie.Delete, Name: "sysproxy.c"},
+			},
+		},
+		{
+			"https://github.com/rumpkernel/rumprun-xen.git",
+			"1831e47b0c6db750714cd0e4be97b5af17fb1eb0",
+			"e13e678f7ee9badd01b120889e0ec5fdc8ae3802",
+			[]expectChange{
+				{Action: merkletrie.Modify, Name: "app-tools/rumprun"},
+			},
+		},
+	} {
 		f := fixtures.ByURL(t.repository).One()
 		sto := s.storageFromPackfile(f)
 
@@ -388,43 +346,10 @@ func (s *DiffTreeSuite) TestDiffTree(c *C) {
 		obtained, err := DiffTree(tree1, tree2)
 		c.Assert(err, IsNil,
 			Commentf("subtest %d: unable to calculate difftree: %s", i, err))
-		c.Assert(equalChanges(obtained, t.expected), Equals, true,
+		c.Assert(equalChanges(obtained, t.expected, c), Equals, true,
 			Commentf("subtest:%d\nrepo=%s\ncommit1=%s\ncommit2=%s\nexpected=%s\nobtained=%s",
 				i, t.repository, t.commit1, t.commit2, t.expected, obtained))
 
 		assertChanges(obtained, c)
 	}
-}
-
-func assertChanges(a Changes, c *C) {
-	for _, changes := range a {
-		switch changes.Action {
-		case Insert:
-			c.Assert(changes.From.Tree, IsNil)
-			c.Assert(changes.To.Tree, NotNil)
-		case Delete:
-			c.Assert(changes.From.Tree, NotNil)
-			c.Assert(changes.To.Tree, IsNil)
-		case Modify:
-			c.Assert(changes.From.Tree, NotNil)
-			c.Assert(changes.To.Tree, NotNil)
-		}
-	}
-}
-
-func equalChanges(a Changes, b []expectChange) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	sort.Sort(a)
-
-	for i, va := range a {
-		vb := b[i]
-		if va.Action != vb.Action || va.name() != vb.Name {
-			return false
-		}
-	}
-
-	return true
 }
