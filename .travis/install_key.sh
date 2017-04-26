48d32644cd8e1846b66582f7e508598f77e39ccf
Máximo Cuadros <mcuadros@gmail.com>
2017-04-26 17:00:54 +0200

    travis: fix export private key

--- .travis/install_key.sh
+++ .travis/install_key.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-export SSH_TEST_PRIVATE_KEY=$PWD/.travis/deploy.pem
-
 openssl aes-256-cbc \
     -K $encrypted_1477e58fe67a_key \
     -iv $encrypted_1477e58fe67a_iv \
     -in .travis/deploy.pem.enc \
-    -out .travis/deploy.pem -d
+    -out $HOME/.travis/deploy.pem -d
 
-chmod 600 .travis/deploy.pem
+chmod 600 $HOME/.travis/deploy.pem
 
