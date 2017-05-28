f9dc7b130fcd8fa1ff0f086de89744c8c194c1e5
Ethan Young <ethankyoung@gmail.com>
2017-05-28 09:46:54 -0600

    Use xanzy/ssh-agent to create the ssh agent correctly based on os.

--- plumbing/transport/ssh/auth_method_test.go
+++ plumbing/transport/ssh/auth_method_test.go
@@ -94,6 +94,16 @@ func (s *SuiteCommon) TestPublicKeysCallbackString(c *C) {
 	c.Assert(a.String(), Equals, fmt.Sprintf("user: test, name: %s", PublicKeysCallbackName))
 }
 func (s *SuiteCommon) TestNewSSHAgentAuth(c *C) {
+	if os.Getenv("SSH_AUTH_SOCK") == "" {
+		c.Skip("SSH_AUTH_SOCK or SSH_TEST_PRIVATE_KEY are required")
+	}
+
+	auth, err := NewSSHAgentAuth("foo")
+	c.Assert(err, IsNil)
+	c.Assert(auth, NotNil)
+}
+
+func (s *SuiteCommon) TestNewSSHAgentAuthNoAgent(c *C) {
 	addr := os.Getenv("SSH_AUTH_SOCK")
 	err := os.Unsetenv("SSH_AUTH_SOCK")
 	c.Assert(err, IsNil)
@@ -105,7 +115,7 @@ func (s *SuiteCommon) TestNewSSHAgentAuth(c *C) {
 
 	k, err := NewSSHAgentAuth("foo")
 	c.Assert(k, IsNil)
-	c.Assert(err, Equals, ErrEmptySSHAgentAddr)
+	c.Assert(err, ErrorMatches, ".*SSH_AUTH_SOCK.*")
 }
 
 func (*SuiteCommon) TestNewPublicKeys(c *C) {
