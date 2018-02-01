0d1e52eaa4f464abb84c101b062705b226a8976f
ferhat elmas <elmas.ferhat@gmail.com>
2018-02-01 22:21:44 +0100

    storage/filesystem/shallow: fix error checking
    Signed-off-by: ferhat elmas <elmas.ferhat@gmail.com>

--- storage/filesystem/shallow.go
+++ storage/filesystem/shallow.go
@@ -26,7 +26,7 @@ func (s *ShallowStorage) SetShallow(commits []plumbing.Hash) error {
 
 	defer ioutil.CheckClose(f, &err)
 	for _, h := range commits {
-		if _, err := fmt.Fprintf(f, "%s\n", h); err != err {
+		if _, err := fmt.Fprintf(f, "%s\n", h); err != nil {
 			return err
 		}
 	}
