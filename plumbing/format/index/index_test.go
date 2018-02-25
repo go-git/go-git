9fb58fc0561855882b1741dbb8b6cbaf6e889351
Máximo Cuadros <mcuadros@gmail.com>
2018-02-26 00:26:37 +0100

    plumbing: format index, Index.Add and Index.Glob methods
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

--- plumbing/format/index/index_test.go
+++ plumbing/format/index/index_test.go
@@ -1,8 +1,21 @@
 package index
 
 import (
+	"path/filepath"
+
 	. "gopkg.in/check.v1"
 )
+
+func (s *IndexSuite) TestIndexAdd(c *C) {
+	idx := &Index{}
+	e := idx.Add("foo")
+	e.Size = 42
+
+	e, err := idx.Entry("foo")
+	c.Assert(err, IsNil)
+	c.Assert(e.Name, Equals, "foo")
+	c.Assert(e.Size, Equals, uint32(42))
+}
 
 func (s *IndexSuite) TestIndexEntry(c *C) {
 	idx := &Index{
@@ -36,4 +49,28 @@ func (s *IndexSuite) TestIndexRemove(c *C) {
 	e, err = idx.Remove("foo")
 	c.Assert(e, IsNil)
 	c.Assert(err, Equals, ErrEntryNotFound)
+}
+
+func (s *IndexSuite) TestIndexGlob(c *C) {
+	idx := &Index{
+		Entries: []*Entry{
+			{Name: "foo/bar/bar", Size: 42},
+			{Name: "foo/baz/qux", Size: 42},
+			{Name: "fux", Size: 82},
+		},
+	}
+
+	m, err := idx.Glob(filepath.Join("foo", "b*"))
+	c.Assert(err, IsNil)
+	c.Assert(m, HasLen, 2)
+	c.Assert(m[0].Name, Equals, "foo/bar/bar")
+	c.Assert(m[1].Name, Equals, "foo/baz/qux")
+
+	m, err = idx.Glob("f*")
+	c.Assert(err, IsNil)
+	c.Assert(m, HasLen, 3)
+
+	m, err = idx.Glob("f*/baz/q*")
+	c.Assert(err, IsNil)
+	c.Assert(m, HasLen, 1)
 }
