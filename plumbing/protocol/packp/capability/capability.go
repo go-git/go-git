83066aedfd72523f353cf8ad95ab1f1b6744cd56
Roberto O. Fernández Crisial <rcrisial@sparkdigital.com>
2020-12-08 01:19:41 -0300

    plumbing: protocol/packp, Adding object-format and filter capabilities. Fixes #220

--- plumbing/protocol/packp/capability/capability.go
+++ plumbing/protocol/packp/capability/capability.go
@@ -233,6 +233,9 @@ const (
 	// ObjectFormat takes a hash algorithm as an argument, indicates that the
 	// server supports the given hash algorithms.
 	ObjectFormat Capability = "object-format"
+	// Filter if present, fetch-pack may send "filter" commands to request a
+	// partial clone or partial fetch and request that the server omit various objects from the packfile
+	Filter Capability = "filter"
 )
 
 const DefaultAgent = "go-git/4.x"
@@ -244,7 +247,7 @@ var known = map[Capability]bool{
 	NoProgress: true, IncludeTag: true, ReportStatus: true, DeleteRefs: true,
 	Quiet: true, Atomic: true, PushOptions: true, AllowTipSHA1InWant: true,
 	AllowReachableSHA1InWant: true, PushCert: true, SymRef: true,
-	ObjectFormat: true,
+	ObjectFormat: true, Filter: true
 }
 
 var requiresArgument = map[Capability]bool{
