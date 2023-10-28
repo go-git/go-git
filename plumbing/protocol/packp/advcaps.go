1fc4c32f3690e27655dd1ce74c3d8f96f714e765
Brian Mayer <bleemayer@gmail.com>
2023-10-28 13:41:37 -0300

    Update plumbing/protocol/packp/advcaps.go
    Co-authored-by: Paulo Gomes <paulo.gomes.uk@gmail.com>

--- plumbing/protocol/packp/advcaps.go
+++ plumbing/protocol/packp/advcaps.go
@@ -57,9 +57,9 @@ func (a *AdvCaps) Decode(r io.Reader) error {
 	f := string(s.Bytes())
 	if i := strings.Index(f, "service="); i < 0 {
 		return fmt.Errorf("missing service indication")
-	} else {
-		a.Service = f[i+8 : len(f)-1]
 	}
+	
+	a.Service = f[i+8 : len(f)-1]
 
 	// scan flush
 	s.Scan()
