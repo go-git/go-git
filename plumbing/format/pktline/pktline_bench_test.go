5a5144adbe5f687b9c699489435dc2ce39ad7e52
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-12-22 18:53:05 -0500

    plumbing: pktline, update benchmarks

--- plumbing/format/pktline/pktline_bench_test.go
+++ plumbing/format/pktline/pktline_bench_test.go
@@ -2,11 +2,55 @@ package pktline_test
 
 import (
 	"bytes"
+	"io"
 	"strings"
 	"testing"
 
 	"github.com/go-git/go-git/v5/plumbing/format/pktline"
 )
+
+func BenchmarkScanner(b *testing.B) {
+	sections, err := sectionsExample(2, 4)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	cases := []struct {
+		name  string
+		input string
+	}{
+		{
+			name:  "empty",
+			input: "",
+		},
+		{
+			name:  "one message",
+			input: "000ahello\n",
+		},
+		{
+			name:  "two messages",
+			input: "000ahello\n000bworld!\n",
+		},
+		{
+			name:  "sections",
+			input: sections.String(),
+		},
+	}
+	for _, tc := range cases {
+		r := strings.NewReader("")
+		s := pktline.NewScanner(r)
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				r.Reset(tc.input)
+				if !s.Scan() {
+					if err := s.Err(); err != nil && err != io.EOF {
+						b.Error(err)
+					}
+				}
+			}
+		})
+	}
+}
 
 func BenchmarkReadPacket(b *testing.B) {
 	sections, err := sectionsExample(2, 4)
@@ -38,9 +82,52 @@ func BenchmarkReadPacket(b *testing.B) {
 	for _, tc := range cases {
 		r := strings.NewReader("")
 		b.Run(tc.name, func(b *testing.B) {
+			buf := pktline.GetPacketBuffer()
 			for i := 0; i < b.N; i++ {
 				r.Reset(tc.input)
-				_, _, err := pktline.ReadPacket(r)
+				_, err := pktline.ReadPacket(r, (*buf)[:])
+				if err != nil && err != io.EOF {
+					b.Error(err)
+				}
+			}
+			pktline.PutPacketBuffer(buf)
+		})
+	}
+}
+
+func BenchmarkReadPacketLine(b *testing.B) {
+	sections, err := sectionsExample(2, 4)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	cases := []struct {
+		name  string
+		input string
+	}{
+		{
+			name:  "empty",
+			input: "",
+		},
+		{
+			name:  "one message",
+			input: "000ahello\n",
+		},
+		{
+			name:  "two messages",
+			input: "000ahello\n000bworld!\n",
+		},
+		{
+			name:  "sections",
+			input: sections.String(),
+		},
+	}
+	for _, tc := range cases {
+		r := strings.NewReader("")
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				r.Reset(tc.input)
+				_, _, err := pktline.ReadPacketLine(r)
 				if err != nil {
 					break
 				}
