c27492aa7080a7daa3a7e3675f14155d321dcd98
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-05-13 10:37:00 -0400

    plumbing: pktline, rename sync buffer getter putter

--- plumbing/format/pktline/sync.go
+++ plumbing/format/pktline/sync.go
@@ -2,24 +2,24 @@ package pktline
 
 import "sync"
 
-var byteSlice = sync.Pool{
+var pktBuffer = sync.Pool{
 	New: func() interface{} {
 		var b [MaxSize]byte
 		return &b
 	},
 }
 
-// GetPacketBuffer returns a *[MaxSize]byte that is managed by a
-// sync.Pool. The initial slice length will be 65520 (65kb).
+// GetBuffer returns a *[MaxSize]byte that is managed by a sync.Pool. The
+// initial slice length will be 65520 (65kb).
 //
 // After use, the *[MaxSize]byte should be put back into the sync.Pool by
-// calling PutByteSlice.
-func GetPacketBuffer() *[MaxSize]byte {
-	buf := byteSlice.Get().(*[MaxSize]byte)
+// calling PutBuffer.
+func GetBuffer() *[MaxSize]byte {
+	buf := pktBuffer.Get().(*[MaxSize]byte)
 	return buf
 }
 
-// PutPacketBuffer puts buf back into its sync.Pool.
-func PutPacketBuffer(buf *[MaxSize]byte) {
-	byteSlice.Put(buf)
+// PutBuffer puts buf back into its sync.Pool.
+func PutBuffer(buf *[MaxSize]byte) {
+	pktBuffer.Put(buf)
 }
