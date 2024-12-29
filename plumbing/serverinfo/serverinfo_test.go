a067bdfceddfa260dea7742aefe9b26586fa52fb
Christophe Gouiran <bechris13250@gmail.com>
2024-12-29 18:09:23 +0100

    plumbing: serverinfo, gocheck to testify migration. Fixes #1305

--- plumbing/serverinfo/serverinfo_test.go
+++ plumbing/serverinfo/serverinfo_test.go
@@ -14,33 +14,35 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/storer"
 	"github.com/go-git/go-git/v5/storage"
 	"github.com/go-git/go-git/v5/storage/memory"
-	. "gopkg.in/check.v1"
+	"github.com/stretchr/testify/suite"
 )
 
-type ServerInfoSuite struct{}
+type ServerInfoSuite struct {
+	suite.Suite
+}
 
-var _ = Suite(&ServerInfoSuite{})
+func TestServerInfoSuite(t *testing.T) {
+	suite.Run(t, new(ServerInfoSuite))
+}
 
-func Test(t *testing.T) { TestingT(t) }
-
-func (s *ServerInfoSuite) TestUpdateServerInfoInit(c *C) {
+func (s *ServerInfoSuite) TestUpdateServerInfoInit() {
 	fs := memfs.New()
 	st := memory.NewStorage()
 	r, err := git.Init(st, fs)
-	c.Assert(err, IsNil)
-	c.Assert(r, NotNil)
+	s.NoError(err)
+	s.NotNil(r)
 
 	err = UpdateServerInfo(st, fs)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 }
 
-func assertInfoRefs(c *C, st storage.Storer, fs billy.Filesystem) {
+func assertInfoRefs(s *ServerInfoSuite, st storage.Storer, fs billy.Filesystem) {
 	refsFile, err := fs.Open("info/refs")
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	defer refsFile.Close()
 	bts, err := io.ReadAll(refsFile)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	localRefs := make(map[plumbing.ReferenceName]plumbing.Hash)
 	for _, line := range strings.Split(string(bts), "\n") {
@@ -48,14 +50,14 @@ func assertInfoRefs(c *C, st storage.Storer, fs billy.Filesystem) {
 			continue
 		}
 		parts := strings.Split(line, "\t")
-		c.Assert(parts, HasLen, 2)
+		s.Len(parts, 2)
 		hash := plumbing.NewHash(parts[0])
 		name := plumbing.ReferenceName(parts[1])
 		localRefs[name] = hash
 	}
 
 	refs, err := st.IterReferences()
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	err = refs.ForEach(func(ref *plumbing.Reference) error {
 		name := ref.Name()
@@ -66,48 +68,48 @@ func assertInfoRefs(c *C, st storage.Storer, fs billy.Filesystem) {
 				return nil
 			}
 			ref, err := st.Reference(ref.Target())
-			c.Assert(err, IsNil)
+			s.NoError(err)
 			hash = ref.Hash()
 			fallthrough
 		case plumbing.HashReference:
 			h, ok := localRefs[name]
-			c.Assert(ok, Equals, true)
-			c.Assert(h, Equals, hash)
+			s.True(ok)
+			s.Equal(hash, h)
 			if name.IsTag() {
 				tag, err := object.GetTag(st, hash)
 				if err == nil {
 					t, ok := localRefs[name+"^{}"]
-					c.Assert(ok, Equals, true)
-					c.Assert(t, Equals, tag.Target)
+					s.True(ok)
+					s.Equal(tag.Target, t)
 				}
 			}
 		}
 		return nil
 	})
 
-	c.Assert(err, IsNil)
+	s.NoError(err)
 }
 
-func assertObjectPacks(c *C, st storage.Storer, fs billy.Filesystem) {
+func assertObjectPacks(s *ServerInfoSuite, st storage.Storer, fs billy.Filesystem) {
 	infoPacks, err := fs.Open("objects/info/packs")
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	defer infoPacks.Close()
 	bts, err := io.ReadAll(infoPacks)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	pos, ok := st.(storer.PackedObjectStorer)
-	c.Assert(ok, Equals, true)
+	s.True(ok)
 	localPacks := make(map[string]struct{})
 	packs, err := pos.ObjectPacks()
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	for _, line := range strings.Split(string(bts), "\n") {
 		if line == "" {
 			continue
 		}
 		parts := strings.Split(line, " ")
-		c.Assert(parts, HasLen, 2)
+		s.Len(parts, 2)
 		pack := strings.TrimPrefix(parts[1], "pack-")
 		pack = strings.TrimSuffix(pack, ".pack")
 		localPacks[pack] = struct{}{}
@@ -115,72 +117,72 @@ func assertObjectPacks(c *C, st storage.Storer, fs billy.Filesystem) {
 
 	for _, p := range packs {
 		_, ok := localPacks[p.String()]
-		c.Assert(ok, Equals, true)
+		s.True(ok)
 	}
 }
 
-func (s *ServerInfoSuite) TestUpdateServerInfoTags(c *C) {
+func (s *ServerInfoSuite) TestUpdateServerInfoTags() {
 	fs := memfs.New()
 	st := memory.NewStorage()
 	r, err := git.Clone(st, fs, &git.CloneOptions{
 		URL: fixtures.ByURL("https://github.com/git-fixtures/tags.git").One().URL,
 	})
-	c.Assert(err, IsNil)
-	c.Assert(r, NotNil)
+	s.NoError(err)
+	s.NotNil(r)
 
 	err = UpdateServerInfo(st, fs)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
-	assertInfoRefs(c, st, fs)
-	assertObjectPacks(c, st, fs)
+	assertInfoRefs(s, st, fs)
+	assertObjectPacks(s, st, fs)
 }
 
-func (s *ServerInfoSuite) TestUpdateServerInfoBasic(c *C) {
+func (s *ServerInfoSuite) TestUpdateServerInfoBasic() {
 	fs := memfs.New()
 	st := memory.NewStorage()
 	r, err := git.Clone(st, fs, &git.CloneOptions{
 		URL: fixtures.Basic().One().URL,
 	})
-	c.Assert(err, IsNil)
-	c.Assert(r, NotNil)
+	s.NoError(err)
+	s.NotNil(r)
 
 	err = UpdateServerInfo(st, fs)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
-	assertInfoRefs(c, st, fs)
-	assertObjectPacks(c, st, fs)
+	assertInfoRefs(s, st, fs)
+	assertObjectPacks(s, st, fs)
 }
 
-func (s *ServerInfoSuite) TestUpdateServerInfoBasicChange(c *C) {
+func (s *ServerInfoSuite) TestUpdateServerInfoBasicChange() {
 	fs := memfs.New()
 	st := memory.NewStorage()
 	r, err := git.Clone(st, fs, &git.CloneOptions{
 		URL: fixtures.Basic().One().URL,
 	})
-	c.Assert(err, IsNil)
-	c.Assert(r, NotNil)
+	s.NoError(err)
+	s.NotNil(r)
 
 	err = UpdateServerInfo(st, fs)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
-	assertInfoRefs(c, st, fs)
-	assertObjectPacks(c, st, fs)
+	assertInfoRefs(s, st, fs)
+	assertObjectPacks(s, st, fs)
 
 	head, err := r.Head()
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	ref := plumbing.NewHashReference("refs/heads/my-branch", head.Hash())
 	err = r.Storer.SetReference(ref)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	_, err = r.CreateTag("test-tag", head.Hash(), &git.CreateTagOptions{
 		Message: "test-tag",
 	})
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
 	err = UpdateServerInfo(st, fs)
-	c.Assert(err, IsNil)
+	s.NoError(err)
 
-	assertInfoRefs(c, st, fs)
-	assertObjectPacks(c, st, fs)
+	assertInfoRefs(s, st, fs)
+	assertObjectPacks(s, st, fs)
 }
