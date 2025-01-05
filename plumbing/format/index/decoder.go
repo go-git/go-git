eb4d0f2a315f76302ce589ee2861d4d21ecfa60f
Paulo Gomes <pjbgf@linux.com>
2025-01-05 17:09:19 +0000

    plumbing: format/index, Support new plumbing.Hash
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- plumbing/format/index/decoder.go
+++ plumbing/format/index/decoder.go
@@ -300,11 +300,11 @@ func (d *Decoder) getExtensionReader() (*bufio.Reader, error) {
 func (d *Decoder) readChecksum(expected []byte) error {
 	var h plumbing.Hash
 
-	if _, err := io.ReadFull(d.r, h[:]); err != nil {
+	if _, err := h.FromReader(d.r); err != nil {
 		return err
 	}
 
-	if !bytes.Equal(h[:], expected) {
+	if h.Compare(expected) != 0 {
 		return ErrInvalidChecksum
 	}
 
@@ -394,7 +394,7 @@ func (d *treeExtensionDecoder) readEntry() (*TreeEntry, error) {
 	}
 
 	e.Trees = i
-	_, err = io.ReadFull(d.r, e.Hash[:])
+	_, err = e.Hash.FromReader(d.r)
 	if err != nil {
 		return nil, err
 	}
@@ -440,7 +440,7 @@ func (d *resolveUndoDecoder) readEntry() (*ResolveUndoEntry, error) {
 
 	for s := range e.Stages {
 		var hash plumbing.Hash
-		if _, err := io.ReadFull(d.r, hash[:]); err != nil {
+		if _, err := hash.FromReader(d.r); err != nil {
 			return nil, err
 		}
 
@@ -479,7 +479,7 @@ func (d *endOfIndexEntryDecoder) Decode(e *EndOfIndexEntry) error {
 		return err
 	}
 
-	_, err = io.ReadFull(d.r, e.Hash[:])
+	_, err = e.Hash.FromReader(d.r)
 	return err
 }
 
