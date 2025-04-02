518188c4d9f20b24397d4052e2d552eaeb1db55d
Adrian Edwards <adrian@adriancedwards.com>
2025-04-01 22:40:29 -0400

    tests

--- plumbing/object/commit_walker_test.go
+++ plumbing/object/commit_walker_test.go
@@ -192,6 +192,53 @@ func (s *CommitWalkerSuite) TestCommitPostIteratorWithIgnore() {
 	}
 }
 
+func (s *CommitWalkerSuite) TestCommitPostIteratorNoMerge(c *C) {
+	commit := s.commit(c, plumbing.NewHash(s.Fixture.Head))
+
+	var commits []*Commit
+	NewCommitPostorderIterNoMerge(commit, nil).ForEach(func(c *Commit) error {
+		commits = append(commits, c)
+		return nil
+	})
+
+	c.Assert(commits, HasLen, 6)
+
+	expected := []string{
+		"6ecf0ef2c2dffb796033e5a02219af86ec6584e5",
+		"918c48b83bd081e863dbe1b80f8998f058cd8294",
+		"af2d6a6954d532f8ffb47615169c8fdf9d383a1a",
+		"1669dce138d9b841a518c64b10914d88f5e488ea",
+		"35e85108805c84807bc66a02d91535e1e24b38b9",
+		"b029517f6300c2da0f4b651b8642506cd6aaf45d",
+	}
+
+	for i, commit := range commits {
+		c.Assert(commit.Hash.String(), Equals, expected[i])
+	}
+}
+
+func (s *CommitWalkerSuite) TestCommitPostIteratorWithIgnoreNoMerge(c *C) {
+	commit := s.commit(c, plumbing.NewHash(s.Fixture.Head))
+
+	var commits []*Commit
+	NewCommitPostorderIterNoMerge(commit, []plumbing.Hash{
+		plumbing.NewHash("af2d6a6954d532f8ffb47615169c8fdf9d383a1a"),
+	}).ForEach(func(c *Commit) error {
+		commits = append(commits, c)
+		return nil
+	})
+
+	c.Assert(commits, HasLen, 2)
+
+	expected := []string{
+		"6ecf0ef2c2dffb796033e5a02219af86ec6584e5",
+		"918c48b83bd081e863dbe1b80f8998f058cd8294",
+	}
+	for i, commit := range commits {
+		c.Assert(commit.Hash.String(), Equals, expected[i])
+	}
+}
+
 func (s *CommitWalkerSuite) TestCommitCTimeIterator() {
 	commit := s.commit(plumbing.NewHash(s.Fixture.Head))
 
