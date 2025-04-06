53cef42bc0ebce785e2c4d24dfbf9585f1d243fc
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-04-06 14:24:10 +0300

    plumbing: transport, fix advertise protocol version
    This commit fixes the protocol version advertisement for the ReceivePack
    and UploadPack services. The version should be advertised during the
    advertisement phase if and only if we're advertising references during
    the ReceivePack or UploadPack routines.
    
    This will also change the behavior of the AdvertiseReferences function to
    only accept UploadPackService and ReceivePackService as valid services.
    
    It will also error out if the protocol version is not supported.

---
+++ plumbing/transport/upload_pack_test.go
@@ -0,0 +1,24 @@
+package transport
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type UploadPackSuite struct {
+	suite.Suite
+}
+
+func TestUploadPackSuite(t *testing.T) {
+	suite.Run(t, new(UploadPackSuite))
+}
+
+func (s *UploadPackSuite) TestUploadPackAdvertiseV0() {
+	testAdvertise(s.T(), UploadPack, "", false)
+}
+
+func (s *UploadPackSuite) TestUploadPackAdvertiseV1() {
+	buf := testAdvertise(s.T(), UploadPack, "version=1", false)
+	s.Containsf(buf.String(), "version 1", "advertisement should contain version 1")
+}
