7eb37d183672e84f0ce0a239659f5ec2a5ca5ec7
OlshaMB <michaelolshanskiy@icloud.com>
2025-04-13 20:52:37 +0300

    plumbing: transport, add test cases, to prevent future regression

--- plumbing/transport/receive_pack_test.go
+++ plumbing/transport/receive_pack_test.go
@@ -18,6 +18,11 @@ func (s *ReceivePackSuite) TestReceivePackAdvertiseV0() {
 	testAdvertise(s.T(), ReceivePack, "", false)
 }
 
+func (s *ReceivePackSuite) TestReceivePackAdvertiseV2() {
+	// TODO: support version 2
+	testAdvertise(s.T(), UploadPack, "version=2", false)
+}
+
 func (s *ReceivePackSuite) TestReceivePackAdvertiseV1() {
 	buf := testAdvertise(s.T(), ReceivePack, "version=1", false)
 	s.Containsf(buf.String(), "version 1", "advertisement should contain version 1")
--- plumbing/transport/upload_pack_test.go
+++ plumbing/transport/upload_pack_test.go
@@ -18,6 +18,11 @@ func (s *UploadPackSuite) TestUploadPackAdvertiseV0() {
 	testAdvertise(s.T(), UploadPack, "", false)
 }
 
+func (s *UploadPackSuite) TestUploadPackAdvertiseV2() {
+	// TODO: support version 2
+	testAdvertise(s.T(), UploadPack, "version=2", false)
+}
+
 func (s *UploadPackSuite) TestUploadPackAdvertiseV1() {
 	buf := testAdvertise(s.T(), UploadPack, "version=1", false)
 	s.Containsf(buf.String(), "version 1", "advertisement should contain version 1")
