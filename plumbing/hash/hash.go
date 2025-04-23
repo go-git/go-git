c1790f59336032745f7c3427ec6e4e4985cc5677
Paulo Gomes <pjbgf@linux.com>
2025-04-23 11:10:27 +0100

    plumbing: hash, Bump module to v6
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- plumbing/hash/hash.go
+++ plumbing/hash/hash.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"hash"
 
-	format "github.com/go-git/go-git/v5/plumbing/format/config"
+	format "github.com/go-git/go-git/v6/plumbing/format/config"
 	"github.com/pjbgf/sha1cd"
 )
 
