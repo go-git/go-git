c0c8fbe414d42f6326112d73faeb54abf2ad120f
onee-only <kimww0306@gmail.com>
2025-09-08 01:24:08 +0900

    utils: convert, Implement CRLF converter

---
+++ utils/convert/eol_test.go
@@ -0,0 +1,67 @@
+package convert
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCRLFToLFConverter(t *testing.T) {
+	tests := []struct {
+		input  []byte
+		output []byte
+	}{
+		{
+			input:  []byte("CRLF\r\nto\r\nLF"),
+			output: []byte("CRLF\nto\nLF"),
+		},
+		{
+			input:  []byte("\r\n\r\n\r\n\r\n\r\n"),
+			output: []byte("\n\n\n\n\n"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(string(test.input), func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			conv := NewLFWriter(buf)
+
+			n, err := conv.Write(test.input)
+			require.NoError(t, err)
+
+			assert.Equal(t, len(test.input), n)
+			assert.Equal(t, test.output, buf.Bytes())
+		})
+	}
+}
+
+func TestLFToCRLFConverter(t *testing.T) {
+	tests := []struct {
+		input  []byte
+		output []byte
+	}{
+		{
+			input:  []byte("LF\nto\nCRLF"),
+			output: []byte("LF\r\nto\r\nCRLF"),
+		},
+		{
+			input:  []byte("\n\n\n\n\n"),
+			output: []byte("\r\n\r\n\r\n\r\n\r\n"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(string(test.input), func(t *testing.T) {
+			buf := bytes.NewBuffer(nil)
+			conv := NewCRLFWriter(buf)
+
+			n, err := conv.Write(test.input)
+			require.NoError(t, err)
+
+			assert.Equal(t, len(test.input), n)
+			assert.Equal(t, test.output, buf.Bytes())
+		})
+	}
+}
