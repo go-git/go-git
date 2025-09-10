91ecbe6491136bbaa5793c0346c8b6f9fe3691b4
onee-only <kimww0306@gmail.com>
2025-09-10 14:02:41 +0900

    utils: sync, Fix zlib reader test
    Putting empty byte array to the pool has potential to interrupt other tests
    as it isn't intended size(32kb). Therefore use byte array from the pool instead
    of creating one.
    
    Also all the tests in this package is flaky. sync.Pool doesn't ensure that the
    object we put in will still exist until we get it back. Document the behavior
    for now, so we can decide whether to remove the tests, or consider other approaches.

--- utils/sync/zlib_test.go
+++ utils/sync/zlib_test.go
@@ -8,25 +8,34 @@ import (
 )
 
 func TestGetAndPutZlibReader(t *testing.T) {
+	// FIXME: All the tests using sync.Pool are flaky.
+	// There is always a chance we don't get the object we want.
+	// from sync.Pool:
+	// // Any item stored in the Pool may be removed automatically at any time without
+	// // notification. If the Pool holds the only reference when this happens, the
+	// // item might be deallocated.
 	_, err := GetZlibReader(bytes.NewReader(zlibInitBytes))
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
 
-	dict := &[]byte{}
-	reader := &FakeZLibReader{}
-	PutZlibReader(&ZLibReader{dict: dict, reader: reader})
+	dict := GetByteSlice()
+	defer PutByteSlice(dict)
+	z1 := &ZLibReader{dict: dict, reader: &FakeZLibReader{}}
+	PutZlibReader(z1)
 
 	z2, err := GetZlibReader(bytes.NewReader(zlibInitBytes))
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
+		return
 	}
 
-	if reader != z2.reader {
+	if z1 != z2 {
 		t.Errorf("zlib reader was not reused")
+		return
 	}
 
-	if !reader.wasReset {
+	if !z2.reader.(*FakeZLibReader).wasReset {
 		t.Errorf("reader was not reset")
 	}
 }
