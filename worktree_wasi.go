bc17c692125ed6674c2ba477480af8f784eef2a0
Catherine <whitequark@whitequark.org>
2025-11-05 23:40:07 +0000

    git: worktree, add `GOOS=wasip1` support.

---
+++ worktree_wasi.go
@@ -0,0 +1,31 @@
+//go:build wasip1
+// +build wasip1
+
+package git
+
+import (
+	"syscall"
+	"time"
+
+	"github.com/go-git/go-git/v6/plumbing/format/index"
+)
+
+func init() {
+	fillSystemInfo = func(e *index.Entry, sys interface{}) {
+		if os, ok := sys.(*syscall.Stat_t); ok {
+			e.CreatedAt = time.Unix(int64(os.Ctime), 0)
+			e.Dev = uint32(os.Dev)
+			e.Inode = uint32(os.Ino)
+			e.GID = os.Gid
+			e.UID = os.Uid
+		}
+	}
+}
+
+func isSymlinkWindowsNonAdmin(_ error) bool {
+	return false
+}
+
+func preReceiveHook(_ string) []byte {
+	return []byte{}
+}
