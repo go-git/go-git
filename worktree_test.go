7b5a30237d7c410a55855e1ec45cd2160e4cf362
Cedric BAIL <cedric.bail@appdirect.com>
2025-11-26 16:33:50 -0700

    git: worktree, ensure TestCheckoutFastPathVerification trigger the checkout fast path

--- worktree_test.go
+++ worktree_test.go
@@ -4023,6 +4023,9 @@ func (s *WorktreeSuite) TestCheckoutFastPathVerification() {
 		Filesystem: memfs.New(),
 	}
 
+	err := w.Checkout(&CheckoutOptions{})
+	s.NoError(err)
+
 	head, err := w.r.Head()
 	s.NoError(err)
 
