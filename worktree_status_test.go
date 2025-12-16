383fdb9ad677015521a4e3a6e19eb69328837d0d
onee-only <kimww0306@gmail.com>
2025-12-16 20:05:31 +0900

    git: worktree_status, Improve status benchmark

--- worktree_status_test.go
+++ worktree_status_test.go
@@ -2,17 +2,22 @@ package git
 
 import (
 	"os"
+	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"testing"
 
+	"github.com/go-git/go-billy/v6"
 	"github.com/go-git/go-billy/v6/memfs"
 	"github.com/go-git/go-billy/v6/osfs"
+	"github.com/go-git/go-billy/v6/util"
 	fixtures "github.com/go-git/go-git-fixtures/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/go-git/go-git/v6/plumbing/cache"
+	"github.com/go-git/go-git/v6/plumbing/format/index"
 	"github.com/go-git/go-git/v6/storage/filesystem"
 )
 
@@ -93,23 +98,151 @@ func TestIndexEntrySizeUpdatedForNonRegularFiles(t *testing.T) {
 }
 
 func BenchmarkWorktreeStatus(b *testing.B) {
-	b.StopTimer()
+	repositories := []struct {
+		name string
+		new  func(b *testing.B) *Repository
+		run  func() bool
+	}{
+		{
+			name: "basic-fixture(memfs)",
+			run:  func() bool { return true },
+			new: func(b *testing.B) *Repository {
+				f := fixtures.Basic().One()
+				st := filesystem.NewStorage(f.DotGit(), cache.NewObjectLRUDefault())
 
-	f := fixtures.Basic().One()
-	st := filesystem.NewStorage(f.DotGit(), cache.NewObjectLRUDefault())
+				r, err := Open(st, memfs.New())
+				require.NoError(b, err)
 
-	r, err := Open(st, memfs.New())
-	require.NoError(b, err)
+				wt, err := r.Worktree()
+				require.NoError(b, err)
 
-	wt, err := r.Worktree()
-	require.NoError(b, err)
+				err = wt.Reset(&ResetOptions{Mode: HardReset})
+				require.NoError(b, err)
 
-	err = wt.Reset(&ResetOptions{Mode: HardReset})
-	require.NoError(b, err)
+				return r
+			},
+		},
 
-	b.StartTimer()
+		{
+			name: "basic-fixture(osfs)",
+			run:  func() bool { return true },
+			new: func(b *testing.B) *Repository {
+				temp := b.TempDir()
 
-	for b.Loop() {
-		wt.Status()
+				f := fixtures.Basic().One()
+				dotgit := f.DotGit(fixtures.WithTargetDir(func() string {
+					return filepath.Join(temp, GitDirName)
+				}))
+
+				st := filesystem.NewStorage(dotgit, cache.NewObjectLRUDefault())
+
+				r, err := Open(st, osfs.New(temp))
+				require.NoError(b, err)
+
+				wt, err := r.Worktree()
+				require.NoError(b, err)
+
+				err = wt.Reset(&ResetOptions{Mode: HardReset})
+				require.NoError(b, err)
+
+				return r
+			},
+		},
+		{
+			name: "linux-kernel",
+			new: func(b *testing.B) *Repository {
+				r, err := PlainOpen("./tests/testdata/repos/linux")
+				require.NoError(b, err)
+
+				return r
+			},
+			run: func() bool {
+				_, err := os.Stat("./tests/testdata/repos/linux")
+				return err == nil
+			},
+		},
 	}
+
+	b.Run("unmodified(default)", func(b *testing.B) {
+		for _, repo := range repositories {
+			if !repo.run() {
+				continue
+			}
+
+			b.Run(repo.name, func(b *testing.B) {
+				r := repo.new(b)
+
+				wt, err := r.Worktree()
+				require.NoError(b, err)
+
+				for b.Loop() {
+					wt.StatusWithOptions(StatusOptions{Strategy: Preload})
+				}
+			})
+		}
+	})
+
+	b.Run("ignored-files", func(b *testing.B) {
+		addFiles := func(b *testing.B, fs billy.Filesystem) {
+			count := 10_000
+			base := "ignored"
+
+			require.NoError(b, fs.MkdirAll(base, os.ModeDir|os.ModePerm))
+
+			for i := range count {
+				path := path.Join(base, strconv.Itoa(i)+".jar") // *.jar is ignored in basic fixture.
+
+				err := util.WriteFile(fs, path, []byte(path), 0o755)
+				require.NoError(b, err)
+			}
+		}
+
+		for _, repo := range repositories {
+			if !strings.Contains(repo.name, "basic-fixture") || !repo.run() {
+				// We can only run this in basic repositories.
+				continue
+			}
+
+			b.Run(repo.name, func(b *testing.B) {
+				r := repo.new(b)
+
+				wt, err := r.Worktree()
+				require.NoError(b, err)
+
+				addFiles(b, wt.Filesystem)
+
+				for b.Loop() {
+					wt.StatusWithOptions(StatusOptions{Strategy: Preload})
+				}
+			})
+		}
+	})
+
+	b.Run("all-changed", func(b *testing.B) {
+		for _, repo := range repositories {
+			if !repo.run() {
+				continue
+			}
+
+			b.Run(repo.name, func(b *testing.B) {
+				r := repo.new(b)
+
+				// Remove all the files from the worktree.
+				// TODO: use wt.Remove() when it supports index.
+				idx, err := r.Storer.Index()
+				require.NoError(b, err)
+				newIdx := &index.Index{Version: idx.Version}
+
+				b.Cleanup(func() { require.NoError(b, r.Storer.SetIndex(idx)) })
+				require.NoError(b, r.Storer.SetIndex(newIdx))
+
+				wt, err := r.Worktree()
+				require.NoError(b, err)
+
+				for b.Loop() {
+					wt.StatusWithOptions(StatusOptions{Strategy: Preload})
+				}
+			})
+		}
+	})
 }
