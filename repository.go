829f321e193304d3d890294bc6fe8ed1ff8d43bf
Paulo Gomes <pjbgf@linux.com>
2026-02-16 23:00:08 +0000

    git: Add strict checks for supported extensions The upstream Git enforces fail-safe heuristics to ensure that older git versions will avoid handling repositories using extensions they are unaware of.
    The logic is largely based on the value of core.repositoryformatversion. As per
    official Git docs:
    > This version specifies the rules for operating on the on-disk repository data.
    > An implementation of git which does not understand a particular version
    > advertised by an on-disk repository MUST NOT operate on that repository;
    > doing so risks not only producing wrong results, but actually losing data.
    
    Now go-git will ensure that:
    - The git.Open logic will verify and enforces the extension support rules.
    - go-git will keep track of built-in extensions that it supports.
    - Any extension that is Storer-specific (e.g. objectformat, reftables),
      will be confirmed by the specific Storer via new x/storage.ExtensionChecker
      interface. Failing to do so, it will be assumed that the extension is
      unsupported and the Open operation will return an error.
    
    This is a breaking change and it will force go-git to not be able to open
    repositories that it in fact doesn't really support. Conversaly, the error
    messages will be more useful (e.g. unknown extension vs object not found).
    
    Upstream refs:
    - https://git-scm.com/docs/git-config#Documentation/git-config.txt-extensions
    - https://git-scm.com/docs/gitrepository-layout#_git_repository_format_versions
    
    Signed-off-by: Paulo Gomes <pjbgf@linux.com>

--- repository.go
+++ repository.go
@@ -32,7 +32,6 @@ import (
 	"github.com/go-git/go-git/v6/storage/filesystem/dotgit"
 	"github.com/go-git/go-git/v6/utils/ioutil"
 	"github.com/go-git/go-git/v6/utils/trace"
-	xstorage "github.com/go-git/go-git/v6/x/storage"
 )
 
 // GitDirName this is a special folder where all the git stuff is.
@@ -260,19 +259,14 @@ func Open(s storage.Storer, worktree billy.Filesystem) (*Repository, error) {
 		return nil, ErrRepositoryNotExists
 	}
 
+	cfg, err := s.Config()
 	if err != nil {
 		return nil, err
 	}
 
-	// Fail-safe to avoid data corruption when off-tree storage implementations
-	// are used to open SHA256 repositories which they may not support.
-	if cfg, err := s.Config(); err == nil {
-		if cfg.Extensions.ObjectFormat != formatcfg.UnsetObjectFormat &&
-			cfg.Extensions.ObjectFormat != formatcfg.SHA1 {
-			if _, ok := s.(xstorage.ObjectFormatGetter); !ok {
-				return nil, errors.New("storage do not support SHA256 object format")
-			}
-		}
+	err = verifyExtensions(s, cfg)
+	if err != nil {
+		return nil, err
 	}
 
 	return newRepository(s, worktree), nil
